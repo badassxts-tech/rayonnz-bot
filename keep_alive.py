d9af474ff485451025d17c8eff2a6d41f2278ef8
badassxts-tech <badassxts@gmail.com>
2026-09-02 09:06:59 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from threading import Thread
 import os
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bot is alive!"
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is alive!")
 
 def run():
     port = int(os.getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    server = HTTPServer(('0.0.0.0', port), Handler)
+    server.serve_forever()
 
 def keep_alive():
     t = Thread(target=run)
